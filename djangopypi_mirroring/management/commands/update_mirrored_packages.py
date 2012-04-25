c99e92731ea98342c6de70f42a992c200be5c732
Jason Kraus <zbyte64@gmail.com>
2012-04-25 13:45:26 -0700

    update_mirrored_packages may now initially populate a mirrored index

--- djangopypi_mirroring/management/commands/update_mirrored_packages.py
+++ djangopypi_mirroring/management/commands/update_mirrored_packages.py
@@ -22,27 +22,31 @@ official pypi list url"""
     def handle(self, *args, **options):
         for index in MasterIndex.objects.all():
             rpc = ServerProxy(index.url)
+            #TODO make a full fetch an option
             try:
+                assert False 
                 last = index.logs.latest()
+                last_created = last.created.timetuple()
             except:
                 print 'Error getting latest update for %s' % (str(index),)
-                continue
+                newer = MirrorLog.objects.create(master=index, created=datetime.now())
+                self.update_via_list_packages(rpc, index, newer)
+            else:
+                newer = MirrorLog.objects.create(master=index, created=datetime.now())
+                self.update_via_changelog(rpc, index, newer, last_created)
+    
+    def update_via_list_packages(self, rpc, index, newer):
+        print 'Looking at all packages'
+        for pkg_name in rpc.list_packages():
+            package, created = Package.objects.get_or_create(name=pkg_name)
             
-            newer = MirrorLog.objects.create(master=index, created=datetime.now())
-            
-            print 'Looking at changes since: %d' % (mktime(last.created.timetuple()),)
-            for update in rpc.changelog(int(mktime(last.created.timetuple()))):
-                print str(update)
-                package, created = Package.objects.get_or_create(name=update[0])
+            for pkg_version in rpc.package_releases(pkg_name):
+                release, created = Release.objects.get_or_create(
+                    package=package, version=pkg_version)
                 
-                if update[3] == 'new release':
-                    release, created = Release.objects.get_or_create(
-                        package=package, version=update[1])
-                    
-                    if created:
-                        newer.releases_added.add(release)
-                    
-                    pkg_data = rpc.release_data(update[0],update[1])
+                if created:
+                    newer.releases_added.add(release)
+                    pkg_data = rpc.release_data(pkg_name, pkg_version)
                     print 'Retrieved release data: %s' % (str(pkg_data),)
                     if 'name' in pkg_data:
                         del pkg_data['name']
@@ -56,27 +60,56 @@ official pypi list url"""
                             release.package_info.setlist(key, value)
                     
                     release.save()
-                elif update[3] == 'remove':
-                    Release.objects.filter(package=package, version=update[1]).delete()
-                elif update[3].startswith('add source file'):
-                    try:
-                        release = Release.objects.get(package=package,
-                                                      version=update[1])
-                    except Release.DoesNotExist:
-                        continue
-                    downloads = rpc.release_urls(update[0], update[1])
-                    for download in downloads:
-                        print 'Download data: %s' % (str(download),)
-                        dist, created = Distribution.objects.get_or_create(release=release,
-                            filetype=download['packagetype'],
-                            pyversion=download['python_version'])
-                        
-                        if not created and dist.md5_digest != download['md5_digest']:
-                            dist.md5_digest = download['md5_digest']
-                            dist.content = File(urllib.urlopen(download['url']))
-                        
-                        dist.comment = download['comment_text']
-                        
-                        dist.save()
+            
+    
+    def update_via_changelog(self, rpc, index, newer, last_created):
+        print 'Looking at changes since: %d' % (mktime(last_created),)
+        for update in rpc.changelog(int(mktime(last_created))):
+            print str(update)
+            package, created = Package.objects.get_or_create(name=update[0])
+            
+            if update[3] == 'new release':
+                release, created = Release.objects.get_or_create(
+                    package=package, version=update[1])
+                
+                if created:
+                    newer.releases_added.add(release)
+                
+                pkg_data = rpc.release_data(update[0],update[1])
+                print 'Retrieved release data: %s' % (str(pkg_data),)
+                if 'name' in pkg_data:
+                    del pkg_data['name']
+                if 'version' in pkg_data:
+                    del pkg_data['version']
+                
+                for key, value in pkg_data.iteritems():
+                    if key != 'classifier':
+                        release.package_info[key] = value
+                    else:
+                        release.package_info.setlist(key, value)
+                
+                release.save()
+            elif update[3] == 'remove':
+                Release.objects.filter(package=package, version=update[1]).delete()
+            elif update[3].startswith('add source file'):
+                try:
+                    release = Release.objects.get(package=package,
+                                                  version=update[1])
+                except Release.DoesNotExist:
+                    continue
+                downloads = rpc.release_urls(update[0], update[1])
+                for download in downloads:
+                    print 'Download data: %s' % (str(download),)
+                    dist, created = Distribution.objects.get_or_create(release=release,
+                        filetype=download['packagetype'],
+                        pyversion=download['python_version'])
+                    
+                    if not created and dist.md5_digest != download['md5_digest']:
+                        dist.md5_digest = download['md5_digest']
+                        dist.content = File(urllib.urlopen(download['url']))
+                    
+                    dist.comment = download['comment_text']
+                    
+                    dist.save()
                 
         
